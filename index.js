186baac1fc71bb1d464276c60ca2dcf53af43802
ElenaHellen <golomako.lena@gmail.com>
2021-04-04 22:00:01 +0300

    created new hw

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let x = true + false
+let y = 12 / "6"
+let a = "number" + 15 + 3
+let b = 5 + 3 + "number"
+let c = [1] > null
+let f = "foo" + + "bar"
+let d = 'true' == true
+let e = false == 'false'
+let g = null == ''
+let w = !!"false" == !!"true"
+let p = ['x'] == 'x'
+let l = [] + null + 1
+let n = 0 || "0" && {}
+let m = [1,2,3] == [1,2,3]
+console.log('task1', x ,y,a,b,c,f,d,e,g,w,p,l,n,m)
