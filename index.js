5b7cdab524e36cec09c3efa657612c5fa0f526f2
ElenaHellen <golomako.lena@gmail.com>
2021-04-04 22:14:50 +0300

    created new task

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-
 let x = true + false
+console.log('task 1:', '1)', x)
 let y = 12 / "6"
+console.log('task 1:', '2)', y)
 let a = "number" + 15 + 3
+console.log('task 1:', '3)', a)
 let b = 5 + 3 + "number"
+console.log('task 1:', '4)', b)
 let c = [1] > null
+console.log('task 1:', '5)', c)
 let f = "foo" + + "bar"
+console.log('task 1:', '6)', f)
 let d = 'true' == true
+console.log('task 1:', '7)', d)
 let e = false == 'false'
+console.log('task 1:', '8)', e)
 let g = null == ''
+console.log('task 1:', '9)', g)
 let w = !!"false" == !!"true"
+console.log('task 1:', '10)', w)
 let p = ['x'] == 'x'
+console.log('task 1:', '11)', p)
 let l = [] + null + 1
+console.log('task 1:', '12)', l)
 let n = 0 || "0" && {}
+console.log('task 1:', '13)', n)
 let m = [1,2,3] == [1,2,3]
-console.log('task1', x ,y,a,b,c,f,d,e,g,w,p,l,n,m)
+console.log('task 1:', '14)', m)
+
+let i = 3;
+while (i) {
+  alert( i-- );
+}
+
+let allNum = 40
+while (allNum < 92) {
+    console.log(allNum)
+}
