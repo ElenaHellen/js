acbf23397ebcafc90c9d02f7ddbfeead9b2cdbca
ElenaHellen <golomako.lena@gmail.com>
2021-04-11 18:26:01 +0300

    created new hw4

--- index.js
+++ index.js
@@ -1,62 +1,51 @@
-let arrayStr = ["a", "b", "c"];
-let arrayNum = [1, 2, 3];
-let arrayCombined = arrayStr.concat(arrayNum);
-console.log("Task 1:", arrayCombined);
+//1. Создать объект с днями недели. Ключами в нем должны служить номера дней от начала недели (понедельник - 1-й и т.д.). Вывести текущий день недели.
+let objWeek = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7: 'Sunday'}
+console.log('1)',objWeek[7])
 
-arrayStr.push(1, 2, 3);
-console.log("Task 2:", arrayStr);
+//2. Напишите функцию, которая принимает объект а  возвращает вложенный массив вида [[key, value], [key, value]]
+let objArr = { a: 1, b: 2 };
+let obj = (objArr) => Object.entries(objArr);
+console.log('2)', obj(objArr))
 
-arrayNum.reverse();
-console.log("Task 3:", arrayNum);
+//3.??? Напишите функцию isPlainObject, которая проверяет, является ли элемент именно объектом, а не массивом, null и т.п.
+let objElement = { id: 'Pasha' };
+let isPlainObject = (el) => typeof el === 'object' && !Array.isArray(el) && el !== null;
+console.log('3)', isPlainObject(objElement))
 
-let arrNum = [1, 2, 3, 4, 5];
-let arrNumCutFirst = arrNum.slice(0, 3);
-console.log("Task 4:", arrNumCutFirst);
+//4. Напишите функцию, которая возвращает новый объект без указанных значений.Т.е в качестве параметров функции вы указываете свойства объекта и на выходе вы получите объект без этих свойств.
+const data = { a: 1, b: 2, c: 3 };
+const without = (object, ...args) => {
+  const newObject = { ...object };
 
-let arrNumCutSecond = arrNum.slice(3, 5);
-console.log("Task 5:", arrNumCutSecond);
+  args.forEach((arg) => {
+    delete newObject[arg];
+  });
 
-const arrObj = { js: "test", jq: "hello", css: "world" }
-console.log("Task 6", Object.keys(arrObj))
+  return newObject;
+};
+console.log('4)',without(data, 'b', 'c'));
 
-let arrSumTwoElement = [[1, 2, 3], [4, 5], [6]]
-let arrSum = 0
-for (let i = 0; i < arrSumTwoElement.length; i++) {
-  for (let j = 0; j < arrSumTwoElement[i].length; j++) {
-    arrSum += arrSumTwoElement[i][j]
-  }
-}
-console.log('Task 8:', arrSum)
+//5. Напишите функцию, которая сравнивает два объекта по значениям.
+let a = { a: 1, b: 1 };
+let b = { a: 1, b: 1 };
+console.log('5)', Object.is(a,b)); 
 
-let arrSumThreeElement = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
-let arrSumm = 0
-for (let n = 0; n < arrSumThreeElement.length; n++) {
-  for (let m = 0; m < arrSumThreeElement[n].length; m++) {
-    for (let k = 0; k < arrSumThreeElement[n][m].length; k++) {
-      arrSumm += arrSumThreeElement[n][m][k]
+
+
+const data4 = { a: 1, b: 2 };
+const data5 = { c: 1, b: 2 };
+const intersection = (firstObj, secondObj) => {
+  const firstObjKeys = Object.keys(firstObj);
+
+  return firstObjKeys.reduce((acc = {}, key) => {
+    if (firstObj[key] === secondObj[key]) {
+      acc = {
+        ...acc,
+        [key]: firstObj[key],
+      };
     }
-  }
-}
-console.log('Task 9:', arrSumm)
 
-/*let arrFirst = [1, 2, 3]
-let arrSecond = [1, 2, 3]
-function arrСomparison() {
-    if (arrFirst.length == arrSecond.length) {
-        return true;
-    }
-}
-console.log(arrComparison)
-*/
-
-let arrElement = [1, 2, 3, 4, 5, 6];
-let arrElementMap = arrElement.map(el => el*el);
-console.log('Task 11:', arrElementMap);
-
-let arrNegativeElement = [-2, 1, 2, -30, 4, -1, -26];
-let arrNegative = arrNegativeElement.filter(el => el < 0);
-console.log('Task 12:', arrNegative);
-
-let arrSumElement = arrElement.reduce((el1, el2) => el1 + el2);
-console.log('Task 13:', arrSumElement);
-
+    return acc;
+  }, {});
+};
+console.log(intersection('7)', data4, data5));
