927bff274e0df73c18f22102cdc730a50b02e588
ElenaHellen <golomako.lena@gmail.com>
2021-04-25 22:29:07 +0300

    created new task

--- index.js
+++ index.js
@@ -1,47 +1,7 @@
-
-function buttonClick5() {
-	let btn = document.getElementById('btn');
-	alert(btn.value);
-}
-function buttonClick6() {
-	let input = document.getElementById('input');
-	input.value = 'Ой, поменялся текст';
-}
-function buttonClick14() {
-	let box = document.getElementById('box');
-	box.style.color = 'red'
-	box.style.width =  '20%'
-}
-
-function buttonClick15() {
-	let box1 = document.getElementById('box1');
-	box1.style.display = 'none';
-}
-function buttonClick1() {
-	let box1 = document.getElementById('box1');
-	box1.style.display = 'block';	
-}
-function buttonClick16() {
-	let box2 = document.getElementById('box2');
-	box2.value = 'Ой, я поменял свой текст и css!';
-	box2.style.color = 'red'
-	box2.style.width =  '15%'
-	box2.style.padding = '2%'
-	box2.style.borderRadius = '20px'
-}
-function buttonClick18() {
-	let a = document.getElementById('box4');
-	a.style.width = "10%";
-	a.value ++;
-}
-/*function buttonClick19() {
-	let box5 = document.getElementById('box5');
-	box5.value = 'Теперь я справа'
+let btn = document.getElementById('btn')
+btn.addEventListener('click', func) 
+function func() {
+	setTimeout( () => { console.log('Hello world')}, 5000);
 	
-}*/
-
-function buttonClick20() {
-	let box6 = document.getElementById('box6');
-	box6.value = 'Мои css классы: eee www ddd'
-	box6.style.width = "20%";
-}
+	
+}  
